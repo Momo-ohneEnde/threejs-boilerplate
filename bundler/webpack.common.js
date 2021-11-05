1ee8f3d6598a03cd5fb55204f06d41ee937b0394
Marina Lehmann <mlehma03@students.uni-mainz.de>
2021-11-05 16:48:58 +0100

    tidy up code and add infotext for letters

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -6,6 +6,7 @@ const path = require('path')
 module.exports = {
     // specify path to js file which should be loaded
     entry: path.resolve(__dirname, '../src/script.js'),
+    //entry: path.resolve(__dirname, '../src/experiments.js'),
     //entry: path.resolve(__dirname, '../src/script_periodic.js'),
     //entry: path.resolve(__dirname, '../src/script_goethe.js'),
 
